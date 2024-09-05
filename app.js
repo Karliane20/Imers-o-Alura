6396ecb8c451b7f323428ab10647e656136015f1
Karliane Ferrari <81335302+Karliane20@users.noreply.github.com>
2024-09-05 18:08:21 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+
+    // Itera sobre cada dado da lista de dados
+    for (let dado of criptomoedas) {
+        // Cria um novo elemento HTML para cada resultado
+        resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.nome}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank" class="link-externo">Ver valor da Moeda</a>
+            </div>
+        `;
+    }
+
+    // Atribui os resultados gerados à seção HTML
+    section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,27 @@
+let criptomoedas = [
+    {
+        nome: "Bitcoin",
+        descricao: "A primeira e mais conhecida criptomoeda, muitas vezes considerada o 'ouro digital'. É uma forma de moeda digital descentralizada, operando em uma rede ponto a ponto sem a necessidade de intermediários.",
+        link: "https://coinmarketcap.com/currencies/bitcoin/"
+    },
+    {
+        nome: "Ethereum",
+        descricao: "Uma plataforma de contratos inteligentes que permite a criação de aplicativos descentralizados (dApps). O Ethereum é a segunda maior criptomoeda em termos de capitalização de mercado.",
+        link: "https://coinmarketcap.com/currencies/ethereum/"
+    },
+    {
+        nome: "Tether",
+        descricao: "Uma stablecoin vinculada ao dólar americano, projetada para manter um valor estável e oferecer uma alternativa mais estável às outras criptomoedas.",
+        link: "https://coinmarketcap.com/currencies/tether/"
+    },
+    {
+        nome: "BNB",
+        descricao: "O token nativo da Binance Chain, uma das maiores exchanges de criptomoedas do mundo. O BNB é utilizado para pagar taxas de negociação e acessar serviços exclusivos na plataforma.",
+        link: "https://www.infomoney.com.br/cotacoes/cripto/ativo/binance-coin-bnb/"
+    },
+    {
+        nome: "Solana",
+        descricao: "Uma plataforma de blockchain de alto desempenho, projetada para escalar e suportar um grande número de transações por segundo. A Solana é conhecida por suas taxas baixas e alta velocidade.",
+        link: "https://coinmarketcap.com/currencies/solana/"
+    }
+];
