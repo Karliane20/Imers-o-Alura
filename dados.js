41f988f5c029fcd0475b706f26724b78487cfa09
Karliane Ferrari <81335302+Karliane20@users.noreply.github.com>
2024-09-06 15:47:19 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -2,26 +2,32 @@ let criptomoedas = [
     {
         nome: "Bitcoin",
         descricao: "A primeira e mais conhecida criptomoeda, muitas vezes considerada o 'ouro digital'. É uma forma de moeda digital descentralizada, operando em uma rede ponto a ponto sem a necessidade de intermediários.",
-        link: "https://coinmarketcap.com/currencies/bitcoin/"
+        link: "https://coinmarketcap.com/currencies/bitcoin/",
+        tags: " BTC , Blockchain, btc"
     },
     {
         nome: "Ethereum",
         descricao: "Uma plataforma de contratos inteligentes que permite a criação de aplicativos descentralizados (dApps). O Ethereum é a segunda maior criptomoeda em termos de capitalização de mercado.",
-        link: "https://coinmarketcap.com/currencies/ethereum/"
+        link: "https://coinmarketcap.com/currencies/ethereum/",
+        tags: " Ether, eth,"
     },
     {
         nome: "Tether",
         descricao: "Uma stablecoin vinculada ao dólar americano, projetada para manter um valor estável e oferecer uma alternativa mais estável às outras criptomoedas.",
-        link: "https://coinmarketcap.com/currencies/tether/"
+        link: "https://coinmarketcap.com/currencies/tether/",
+        tags: "Tether stablecoin, USDT reserva, usdt "
     },
     {
         nome: "BNB",
         descricao: "O token nativo da Binance Chain, uma das maiores exchanges de criptomoedas do mundo. O BNB é utilizado para pagar taxas de negociação e acessar serviços exclusivos na plataforma.",
-        link: "https://www.infomoney.com.br/cotacoes/cripto/ativo/binance-coin-bnb/"
+        link: "https://www.infomoney.com.br/cotacoes/cripto/ativo/binance-coin-bnb/",
+        tags: "Binance Coin, bnb"
     },
     {
         nome: "Solana",
         descricao: "Uma plataforma de blockchain de alto desempenho, projetada para escalar e suportar um grande número de transações por segundo. A Solana é conhecida por suas taxas baixas e alta velocidade.",
-        link: "https://coinmarketcap.com/currencies/solana/"
+        link: "https://coinmarketcap.com/currencies/solana/",
+        tags: "solana, sol"
+
     }
 ];
